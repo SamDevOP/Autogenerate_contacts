e8061903a12fc8dea2431f60bf1577422bbf7356
samkinyanjui <samkinyanjui101@gmail.com>
2021-06-01 05:53:33 +0300

    generating random kenyan phon numbers in python

---
+++ random_kenyan_phone_number_generator.py
@@ -0,0 +1,30 @@
+from random import randint
+from random import randint
+
+def genrandom():
+    last=""
+    j=0
+    while j < 8:
+            r= randint(0,9)
+            last+=str(r)
+            j+=1
+    return last
+
+#validate the input 
+#handle the input error
+
+try:
+    contact=int(input("Enter the number of contacts you want: "))
+    for i in range(0,contact):
+        k=genrandom()
+        #print("07" + str(k))
+        print("+254" + str(k))
+   
+        
+
+    #print(type(contact))
+except ValueError:
+    print("You have not entered an integer")
+
+
+
